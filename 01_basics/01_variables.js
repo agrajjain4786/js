a16192766f6e8063818ae1595ef3b41e956c404a
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-02 10:02:55 +0000

    learn variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1905696
+let accountEmail = "agraj@google.com"
+var accountPassword = "12345"
+accountCity = "Agra"
+let accountState;
+
+// accountId = 5 // not allowed
+
+accountEmail = "AJ@aj.com"
+accountPassword = "98765"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
