afbb1b15034daf2792faeac77d564e68c8b10aa6
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-03 06:37:36 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+
+// alert(5+4) // we are using nodejs, not browser
+
+let name = "Agraj" //string
+let age = 23 //number
+let isLoggedIn = false //boolean
+let state; 
+
+
+/*number 
+bigint
+string => ""
+boolean=> true/false
+null=> standalone value
+undefined
+symbol =>unique
+
+
+object
+
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
