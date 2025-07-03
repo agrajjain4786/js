15b4cfa6647d41f4a681b89d3efed8a66cd6d3ef
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-03 07:02:14 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = true
+// console.log(typeof(score));
+
+// let convertInNumber = Number(score);
+// console.log(typeof(convertInNumber));
+
+// console.log(convertInNumber);
+
+/*
+ture => 1
+"33"=>33
+"33ab" => NaN(not a number)
+// */
+// let boolean = undefined
+// console.log(typeof(boolean));
+// console.log(boolean);
+
+// let convertBoolean = Boolean(boolean)
+// console.log(typeof(convertBoolean));
+// console.log(convertBoolean);
+
+/*
+1 => true
+"" =>false
+null =>false
+undefined =>false
+*/
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
