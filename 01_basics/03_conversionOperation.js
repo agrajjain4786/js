67e7eaa3ca3947e2299ce3aa3123c0ac763926dc
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-03 07:18:14 +0000

    operation in conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,8 +26,33 @@ null =>false
 undefined =>false
 */
 
-let someNumber =33
+// let someNumber =33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+///////////////////////////// operation ///////////////////////////\
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Agraj"
+let str2 = " jain"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log("1"+(2+2));
+
+
