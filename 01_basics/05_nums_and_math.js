3b73118db5d14cb451a2db6a5818e8397f36bdcc
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-09 08:09:33 +0000

    number and math

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNum = 23.222
+// console.log(otherNum.toPrecision(2));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++ MATHs +++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,5,6,1,2));
+// console.log(Math.max(4,5,6,1,2));
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
