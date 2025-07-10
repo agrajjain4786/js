b51bddc51529495c09df06d6295d5a0d47ff2597
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-10 10:42:43 +0000

    array/ slice/splice

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+// arrays
+
+const myArr = [1, 2, 3, 4, 5, true, 'Agraj']
+// console.log(myArr[6]);
+const arr1 = new Array(1,2,3,5,654,4) 
+// console.log(arr1);
+const arr = new Array()
+// console.log(arr);
+
+// arr[0] = 'one'
+// console.log(arr);
+ 
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(-1)
+// myArr.shift()
+
+// console.log(myArr.indexOf(5));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C ", myArr);
+
