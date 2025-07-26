119627a8b89a128e9fb476357087d38832fbacf3
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-07-26 16:28:38 +0000

    control flow

---
+++ 04_control_flow/01.js
@@ -0,0 +1,32 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+// < ,> ,<= ,>= ,== ,!= ,===
+
+// if(temperature <= 50){
+//     console.log(`temperature is less then 50`);
+// }else{
+//     console.log(`temperature is more than 50`);
+// }
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power : ${power}`);    
+// }
+
+
+const balance = 1000
+
+// if(balance > 500) console.log(`test`);
+
+// if(balance < 500){
+//     console.log(`less than 500`)
+// }else if (balance < 750) {
+//     console.log(`less than 750`);
+// }else if (balance < 750) {
+//     console.log(`less than 750`);
+// }else{
+//     console.log(`less than 1500`);
+// }
+
