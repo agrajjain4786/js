f2853526c3fb3c9f5676bc93621cd65a775ad1dd
Agraj Jain <154494348+agrajjain4786@users.noreply.github.com>
2025-08-02 13:06:12 +0000

    filter

---
+++ 05_iterations/06_.js
@@ -0,0 +1,27 @@
+// const codin = ["js", "ruby", "java", "python", "cpp"]
+
+// const text = codin.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+// console.log(text);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num) => {
+//     return num > 4
+// })
+
+// const newNums = []
+
+// myNums.forEach((item)=>{
+//     if (item > 4) {
+//         newNums.push(item)
+//     }
+// })
+
+// console.log(newNums);
+
+
